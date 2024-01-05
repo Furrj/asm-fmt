2bd2c987647d882eef3435957daa7ce11909b006
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-05 18:48:05 -0500

    workinng on stripEmptyLines

--- asm-fmt.go
+++ asm-fmt.go
@@ -13,12 +13,27 @@ func main() {
 	}
 	filename := os.Args[1]
 
-	contents, err := getContents(filename)
+	lines, err := getContents(filename)
 	if err != nil {
 		fmt.Printf("Error opening file %s: %+v\n", filename, err)
 		os.Exit(1)
 	}
-	prettyPrint(contents)
+	preamble := getPreamble(lines)
+	fmt.Println(preamble.LineCount)
+	prettyPrint(preamble.Content)
+}
+
+type section struct {
+	LineCount int
+	StartLine int
+	Content   []string
+}
+
+type sections struct {
+	Preamble section
+	Data     []string
+	Bss      []string
+	Text     []string
 }
 
 func getContents(filename string) ([]string, error) {
@@ -30,13 +45,37 @@ func getContents(filename string) ([]string, error) {
 }
 
 func prettyPrint(lines []string) {
-	for i := range arr {
-		arr[i] = strings.TrimSpace(arr[i])
-		fmt.Printf("%d: %s\n", i, arr[i])
+	for i := range lines {
+		lines[i] = strings.TrimSpace(lines[i])
+		fmt.Printf("%d: %s\n", i, lines[i])
 		fmt.Println("---------------------")
 	}
 }
 
-func getSections(lines []string) {
-	fmt.Println("Sections")
+func getPreamble(lines []string) section {
+	var preamble section
+	var content []string
+
+scan:
+	for i, v := range lines {
+		if strings.Contains(strings.ToLower(v), "section .data") {
+			preamble.LineCount = i
+			break scan
+		}
+		content = append(content, v)
+	}
+
+	preamble.StartLine = 1
+	preamble.Content = content
+	return preamble
+}
+
+func stripEmptyLines(lines []string) []string {
+	length := len(lines)
+
+	if lines[length-1] == "" {
+		lines = lines[:length]
+	}
+
+	return lines
 }
